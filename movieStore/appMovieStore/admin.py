0bbd6e6e0a468ddcb9a191a05e0588e0926ca46f
JanireDiazdeEspada <144669436+JanireDiazdeEspada@users.noreply.github.com>
2024-11-17 18:20:37 +0100

    Delete movieStore directory

--- movieStore/appMovieStore/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Pelicula, Director, Genero
-
-# Register your models here.
-admin.site.register(Pelicula)
-admin.site.register(Director)
-admin.site.register(Genero)
